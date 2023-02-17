5cce7f59a373306e64cb05eb18d02ae534cc174e
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-17 12:23:58 +0100

    Timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Chronometre } from "./components/chronometre/chronometre";
 import { Compteur } from "./components/compteur/compteur";
 import { Progressbar } from "./components/progressbar/progressbar";
+import { Timer } from "./components/timer/timer";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Chronometre/>
       <Compteur/>
       <Progressbar/>
+      <Timer/>
     </div>
   );
 }
---
+++ src/components/timer/timer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export const Timer = () => {
+    // Crér un component Timer qui va afficher un compte à rebours. Utiliser le useEffect pour gèrer les secondes à retirer et useState pour gèrer l'affichage du compte à rebours.
+    let [seconds, setSeconds] = useState(10);
+    useEffect(() => {
+        // let Decrement = () => {
+            // for(let i = 10; i > 0; i--){
+            //     setTimeout(() => {
+            //         setSeconds(seconds - 1);
+            //     }, 1000);
+            // }
+        if(seconds > 0){
+            let decrement = setTimeout(() => {
+                setSeconds(seconds - 1)
+            }, 1000) 
+            return () => {
+                clearTimeout(decrement)
+            }
+        }
+       
+    }, [seconds])
+    return (
+        <div className='inline-block bg-red-500 rounded-full p-5 text-5xl text-white font-bold'><span>{seconds != 10 && seconds >= 0 ? `0${seconds}` : seconds}</span></div>
+    )
+}
